1b0b4beb1f5ad99d829b4b84330cb467c11e7494
Jerry Huang <jhuang9240@gmail.com>
2019-10-19 17:51:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 		},
 		:names => {
 			:reading_railroad => {},
-			:pennsylvania_railroad => 2,
+			:pennsylvania_railroad => {},
 			:pennsylvania_railroad => 3,
 			:key4 => 4
 		}
